f7d07e310373e6a1ad17e6a543e44dc7d132af65
Omar Saad <omars105@icloud.com>
2026-09-24 20:09:39 +0300

    add s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">1.2.0"
+  backend "s3" {
+    bucket = "omars-tf-state-bucket"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+  }
+
+}
+
+
 provider "aws" {
   region = var.region
   # Credentials are loaded from environment variables or AWS CLI (~/.aws/credentials)

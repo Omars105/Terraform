9c299f715929df7487d8392c603dd8db815060fd
Omar Saad <omars105@icloud.com>
2026-09-24 18:00:03 +0300

    fix: remove default nginx container and add docker-compose down

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -2,8 +2,6 @@
   sudo yum update -y && sudo yum install -y docker
   sudo systemctl start docker && sudo systemctl enable docker
   sudo usermod -aG docker ec2-user
-  docker run -p 8080:80 -d nginx
-
 
 #download docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
